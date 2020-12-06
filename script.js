0f7441e1cc45895a09ff968e54aeebb720ccfce7
Egor-Gerasimchik <egor.gerasimchik2007@gmail.com>
2020-12-06 18:54:17 +0700

    answer

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
-    $("[data-toggle='popover']").popover();
-    $("[data-toggle='collapse']").collapse();
-})
+    $("[data-toggle='popover']").tooltip();
+
+});
+
+
+$("#btnSelect").click(function (e) { 
+    
+    let select = document.querySelector("select.custom-select")
+    
+    let url = select.options[select.selectedIndex].value + " " + "div#answer";
+
+    document.querySelector("#msgLabel").innerHTML = "Сообщение от пользователя  " + select.options[select.selectedIndex].innerHTML;
+
+    
+    $(".modal-body").load(url, function (response, status, request) {
+        
+        document.querySelector(".modal-body>div").id="";
+        
+        $("#msgBox").modal("show");
+    });
+});
+
